4d686ce1995b59e620e4321015b491eae2e41bba
2017000313-KendrickSantos <k.santos.5adb@gmail.com>
2026-03-23 11:40:46 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,18 +1,44 @@
 ﻿internal class Program
 {
     private static void Main(string[] args)
-    {   //entrada
-        Console.WriteLine("Ingrese un numero: ");
-        int numero = Convert.ToInt32(Console.ReadLine());
-        //salida
-        Console.WriteLine("Numeros positivos y negativos!");
-        if (numero >= 0) {
-            Console.WriteLine("El numero es positivo");
-        }
-        else
-        {
-            Console.WriteLine("El numero es negativo");
-        }
+    {
+        Console.WriteLine("===Mensaje de voz===");
+        Console.WriteLine("MENU PRINCIPAL");
+        Console.WriteLine("Ingrese el mensaje de voz:");
+        int opcion;
+        Console.WriteLine("1-recepcion");
+        Console.WriteLine("2-Vigilancia");
+        Console.WriteLine("3-Mantenimiento");
+        Console.WriteLine("4-Salir");
+        Console.WriteLine("digite la opcion que desea: ");
+        opcion = Convert.ToInt32(Console.ReadLine());
 
+        if(opcion == 1)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Bienvenido al depto de recepcion");
+        }
+        if (opcion == 2)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Bienvenido al depto de Vigilancia");
+        }
+        if (opcion == 3)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Bienvenido al depto de Mantenimiento");
+        }
+        if (opcion != 1 && opcion != 2 && opcion != 3 )
+        {
+            Console.WriteLine("Saliendo del Sistema");
+            Console.Clear();
+            Console.WriteLine("Saliendo del sistema");
+        }
+        Console.WriteLine("Presione cualquier tecla para salir");
+        Console.ReadKey();
+        Environment.Exit(0);
     }
 }
