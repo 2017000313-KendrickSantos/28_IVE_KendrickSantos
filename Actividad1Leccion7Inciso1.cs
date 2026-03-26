e1aa826e26e33d5ab954c8329155cf4a984452bb
2017000313-KendrickSantos <k.santos.5adb@gmail.com>
2026-03-26 13:59:34 -0600

    Update and rename CalcularPromedio.cs to Actividad1Leccion7Inciso1.cs

--- Actividad1Leccion7Inciso1.cs
+++ Actividad1Leccion7Inciso1.cs
@@ -15,6 +15,12 @@
         n5 = Convert.ToInt32(Console.ReadLine());
         double promedio = (n1 + n2 + n3 + n4 + n5) / 5;
         Console.WriteLine("El promedio de la materia " + Materia + " es: " + promedio);
+        if (promedio >= 70) {
+            Console.WriteLine("aprobaste");
+        }
+        else (promedio <= 70) {
+            Console.WriteLine("perdiste");
+        }
 
     }
 }
